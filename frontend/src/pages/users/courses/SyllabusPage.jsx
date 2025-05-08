826cc9c447ef4c82d8d29b3373a9f3e3ad2f8dd8
nivin <bnivin71@gmail.com>
2025-05-09 00:47:43 +0530

    syllabus listing in admin side and user side completed

--- frontend/src/pages/users/courses/SyllabusPage.jsx
+++ frontend/src/pages/users/courses/SyllabusPage.jsx
@@ -1,31 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import MainNavbar from "../../../components/navbar/MainNavbar";
 
 const SyllabusPage = () => {
   const { courseId } = useParams();
   const [syllabus, setSyllabus] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://localhost:8000/courses/course/${courseId}/syllabus/`)
-        .then(response => {
+    axios
+      .get(`http://localhost:8000/courses/course/${courseId}/syllabus/`)
+      .then((response) => {
         console.log("Full response:", response);
         console.log("Syllabus data:", response.data);
         setSyllabus(response.data);
-        })
-        .catch(error => console.error("Error loading syllabus:", error));
+      })
+      .catch((error) => console.error("Error loading syllabus:", error));
   }, [courseId]);
 
   return (
     <div>
-      <h2>Syllabus</h2>
-      <ul>
-        {syllabus.map(item => (
-            
-          <li key={item.id}>{item.topic} - {item.description}</li>
-        ))}
-        
-      </ul>
+      <MainNavbar />
+      
+      {syllabus.map((item) => (
+      <div key={item.id} className="collapse collapse-arrow bg-base-100 border border-base-300">
+        <input type="radio" name="my-accordion-2" defaultChecked />
+        <div className="collapse-title font-semibold">
+          {item.topic}
+        </div>
+        <div className="collapse-content text-sm">
+          {item.description}
+        </div>
+      </div>
+      ))}
+      
     </div>
   );
 };
